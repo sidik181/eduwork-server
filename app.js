ae4905373da6c26dd74a1c4093d8de0bdca567a2
Sidik Komarudiansah <sidik.komarudiansah@gmail.com>
2023-10-18 22:13:21 +0700

    update cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { decodeToken } = require('./middlewares');
-// const cors = require('cors');
+const cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
@@ -25,7 +25,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// app.use(cors);
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
